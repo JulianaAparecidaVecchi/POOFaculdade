36b9ff488832a9f685038d8a082ff3459d0608d5
Juliana <julianaaparecidavecchi@gmail.com>
2025-03-04 18:06:02 -0300

    Exercício questionário.

---
+++ TDEs/TDE1/src/Questionario.java
@@ -0,0 +1,11 @@
+public class Questionario {
+    public static double calc(double x1, double x2){
+        return (x1 * x2);
+    }
+    public static void main(String[] args){
+        double d1 = 1.2132;
+        double d2 = 2.4128;
+
+        System.out.println("O resultado é " + calc(d1,d2));
+    }
+}
