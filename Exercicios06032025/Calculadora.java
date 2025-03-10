1081d57d27cd6795221e3caaea5d9f4e4449066b
Juliana <julianaaparecidavecchi@gmail.com>
2025-03-10 12:10:28 -0300

    Incremento calculadora

--- Exercicios06032025/Calculadora.java
+++ Exercicios06032025/Calculadora.java
@@ -1,37 +1,40 @@
 public class Calculadora{
 	private double primeiroNumero;
 	private double segundoNumero;
+	private double resultado;
+
+	private void setValorOperado(){
+		this.primeiroNumero = resultado;
+	}
 
 	public Calculadora(double primeiroNumero, double segundoNumero){
 		this.primeiroNumero = primeiroNumero;
 		this.segundoNumero = segundoNumero;
+		this.resultado = 0.0;
 	}
 	
-	public double soma(){
-		return primeiroNumero + segundoNumero;
+	private void soma(){
+		resultado = primeiroNumero + segundoNumero;
+		setValorOperado();
 	}
 
-	public double subtracao(){
-		return primeiroNumero - segundoNumero;
+	private void subtracao(){
+		resultado = primeiroNumero - segundoNumero;
+		setValorOperado();
 	}
 
-	public void mostrar(){
-		System.out.println("*****| Calculadora - Valores: " + primeiroNumero + " e " 		+ segundoNumero + "|******");
-		System.out.println("-----| Soma |-----");
-		System.out.println("O resultado da soma de " + primeiroNumero + " + " + 
-		segundoNumero + " = " + soma());
-		System.out.println("-----| Subtração |-----");
-		System.out.println("O resultado da soma de " + primeiroNumero + " - " + 
-		segundoNumero + " = " + subtracao());
-		System.out.println("***********");
+	private void mostrar(){
+		System.out.printf("*****| Resultado : %.2f|******\n", resultado);
 	}
 
 	public static void main(String[] args){
 		Calculadora calc01 = new Calculadora(10,12);
+		calc01.soma();
 		calc01.mostrar();
-
-		Calculadora calc02 = new Calculadora(98,74);
-		calc02.mostrar();
+		calc01.subtracao();
+		calc01.mostrar();
+		calc01.subtracao();
+		calc01.mostrar();
 		
 	}
 }
